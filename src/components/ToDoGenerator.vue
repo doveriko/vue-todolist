b7005fd7b5fea7f338cbf5b988f4078dcad561d9
Doveriko <abelmorpe@gmail.com>
2022-08-08 23:49:04 +0200

    `feat`(*): add sass folder for general styles

--- src/components/ToDoGenerator.vue
+++ src/components/ToDoGenerator.vue
@@ -2,7 +2,7 @@
   <div class="todo-generator">
     <p>Add one element to your list</p>
     <input type="text" />
-    <fa-icon icon="circle-plus" alt="Log out" />
+    <fa-icon icon="circle-plus" alt="Log out" class="icon" />
   </div>
 </template>
 
