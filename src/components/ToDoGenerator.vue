66db4887c172dcd9666c2152870dec8836c5bd41
Doveriko <abelmorpe@gmail.com>
2022-08-09 02:37:31 +0200

    `feat`(*): trigger `addItem` on enter key press

--- src/components/ToDoGenerator.vue
+++ src/components/ToDoGenerator.vue
@@ -6,6 +6,7 @@
       name="address"
       v-model.trim="itemInput"
       placeholder="Enter a new task"
+      @keyup.enter="addItem"
     />
     <fa-icon
       icon="circle-plus"
@@ -29,12 +30,14 @@ export default {
   methods: {
     ...mapActions(["addItemToList"]),
     async addItem() {
-      let item = {
-        id: this.idGenerator(3),
-        input: this.itemInput,
-      };
-      await this.addItemToList(item);
-      this.itemInput = null;
+      if (this.itemInput) {
+        let item = {
+          id: this.idGenerator(3),
+          input: this.itemInput,
+        };
+        await this.addItemToList(item);
+        this.itemInput = null;
+      }
     },
     idGenerator(length) {
       var result = "";
@@ -53,6 +56,7 @@ export default {
 
 <style lang="scss" scoped>
 .icon[disabled] {
+  pointer-events: none;
   color: #999;
 }
 </style>
